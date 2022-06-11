db9a1b5c0a56907c1b8807641e8c6111eb7628d4
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2022-06-11 20:10:16 +0200

    Add support for Swagger 4.12.0

--- swagger_ui_bundle/__init__.py
+++ swagger_ui_bundle/__init__.py
@@ -2,8 +2,8 @@
 
 import os
 
-__author__ = "Daniel Grossmann-Kavanagh"
-__version__ = '0.0.9'
+__author__ = "Daniel Grossmann-Kavanagh, Bartolomé Sánchez Salado"
+__version__ = '0.0.1'
 
 
 def get_path(rel):
@@ -12,13 +12,10 @@ def get_path(rel):
     )
 
 
-swagger_ui_2_2_10_path = get_path("vendor/swagger-ui-2.2.10")
-
-swagger_ui_3_52_0_path = get_path("vendor/swagger-ui-3.52.0")
+swagger_ui_4_12_0_path = get_path("vendor/swagger-ui-4.12.0")
 
 # latest major versions
-swagger_ui_2_path = swagger_ui_2_2_10_path
-swagger_ui_3_path = swagger_ui_3_52_0_path
+swagger_ui_4_path = swagger_ui_4_12_0_path
 
-# default to swagger 3
-swagger_ui_path = swagger_ui_3_path
+# default to swagger 4
+swagger_ui_path = swagger_ui_4_path
