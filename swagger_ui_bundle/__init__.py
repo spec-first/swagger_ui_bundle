d9d2b475fd2f826b035c104f7289d8ec1dc6e2ac
Leonides T. Saguisag, Jr <leonidessaguisagjr@yahoo.com>
2022-10-04 13:17:12 -0700

    Add support for swagger-ui v4.14.2.

--- swagger_ui_bundle/__init__.py
+++ swagger_ui_bundle/__init__.py
@@ -3,7 +3,7 @@
 import os
 
 __author__ = "Daniel Grossmann-Kavanagh"
-__version__ = '0.0.9'
+__version__ = '0.0.10'
 
 
 def get_path(rel):
@@ -16,9 +16,12 @@ swagger_ui_2_2_10_path = get_path("vendor/swagger-ui-2.2.10")
 
 swagger_ui_3_52_0_path = get_path("vendor/swagger-ui-3.52.0")
 
+swagger_ui_4_14_2_path = get_path("vendor/swagger-ui-4.14.2")
+
 # latest major versions
 swagger_ui_2_path = swagger_ui_2_2_10_path
 swagger_ui_3_path = swagger_ui_3_52_0_path
+swagger_ui_4_path = swagger_ui_4_14_2_path
 
 # default to swagger 3
 swagger_ui_path = swagger_ui_3_path
--- tests/test_can_import.py
+++ tests/test_can_import.py
@@ -14,4 +14,6 @@ def test_import_ui_v3():
     from swagger_ui_bundle import swagger_ui_3_path
     open(swagger_ui_3_path + "/index.html")
 
-
+def test_import_ui_v4():
+    from swagger_ui_bundle import swagger_ui_4_path
+    open(swagger_ui_4_path + "/index.html")
