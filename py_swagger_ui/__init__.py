53499b1d7246a74fd1986330d080f969632557b4
Robbe Sneyders <robbe.sneyders@ml6.eu>
2023-01-07 20:11:44 +0100

    Fix path resolving

--- py_swagger_ui/__init__.py
+++ py_swagger_ui/__init__.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
 try:
-    import importlib.resources as importlib_resources
-except ImportError:  # < Python 3.9
     import importlib_resources as importlib_resources
+except ImportError:  # < Python 3.9
+    import importlib.resources as importlib_resources
 
 
-swagger_ui_path: Path = importlib_resources.files("py-swagger-ui") / "vendor/swagger-ui-4.15.5"
+swagger_ui_path: Path = importlib_resources.files("py_swagger_ui") / "vendor/swagger-ui-4.15.5"
--- tests/test_can_import.py
+++ tests/test_can_import.py
@@ -2,4 +2,4 @@
 
 def test_import_ui():
     from py_swagger_ui import swagger_ui_path
-    open(swagger_ui_path + "/index.html")
+    open(swagger_ui_path / "index.html")
